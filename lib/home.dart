5395a071acc04c84a823cf9ebcb4d5ab32245cb4
shreyank.bansod <shreyank.bansod@cionlabs.com>
2024-02-15 14:58:54 +0530

    ignore: unused_import

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 // import 'dart:developer';
 
+// ignore: unused_import
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
