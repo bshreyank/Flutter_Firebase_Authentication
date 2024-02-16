a06a0bb9fa4c8732f9fc2ad7c3b5861cc8af0712
shreyank.bansod <shreyank.bansod@cionlabs.com>
2024-02-16 15:46:36 +0530

    moved the provider funcationality from dashboard to part_1 dart file

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,8 +1,6 @@
-import 'dart:convert';
 import 'package:complete/page_1.dart';
 import 'package:complete/page_2.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 class dashboard extends StatefulWidget {
   const dashboard({super.key});
@@ -10,66 +8,6 @@ class dashboard extends StatefulWidget {
   @override
   State<dashboard> createState() => _dashboardState();
 }
-
-//=================================================>>>>
-//================================================ Provider ====>>>>
-
-class ActivityProvider extends ChangeNotifier {
-  String _activity = '';
-  List<String> list = [];
-  bool isloading = false;
-
-  String get activity => _activity;
-
-  //no need
-  get activities => null;
-
-  // bool get isloading => _isloading;
-
-/*
-  void add(String activity) {
-    list.add(activity);
-    notifyListeners();
-  }
-*/
-
-  void handleDeleteActivity(String activity) {
-    list.remove(activity);
-    notifyListeners();
-  }
-
-  void handleDeleteAll() {
-    list.clear();
-    notifyListeners();
-  }
-
-  Future<void> fetchActivity() async {
-    isloading = true;
-    notifyListeners();
-
-    final response =
-        await http.get(Uri.parse('https://www.boredapi.com/api/activity'));
-
-    isloading = false;
-    notifyListeners();
-
-    final jsonData = json.decode(response.body);
-
-    _activity =
-        jsonData['activity']; // Use 'this.' to refer to the class member
-
-    list.add(activity);
-    notifyListeners();
-  }
-
-  void fetchAdditionalActivities() {
-    for (int i = 0; i < 5; i++) {
-      fetchActivity();
-    }
-  }
-}
-
-//=================================================>>>>
 
 class _dashboardState extends State<dashboard> {
   int _selectedIndex = 0;
@@ -93,7 +31,7 @@ class _dashboardState extends State<dashboard> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('BottomNavigationBar Sample'),
+        title: const Text('Bored Activities'),
       ),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
--- lib/page_1.dart
+++ lib/page_1.dart
@@ -1,64 +1,103 @@
-import 'package:complete/dashboard.dart';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 
-class Page_1 extends StatelessWidget {
+//=================================================>>>>
+//================================================ Provider ====>>>>
+// Represents an activity fetched from the API
+class Activity {
+  final String activity;
+
+  Activity({required this.activity});
+
+  // Factory method to create an Activity object from JSON data
+  factory Activity.fromJson(Map<String, dynamic> json) {
+    return Activity(
+      activity: json['activity'],
+    );
+  }
+}
+
+class ActivityProvider extends ChangeNotifier {
+  final List<Activity> _activities = [];
+
+  // Getter to access the list of activities
+  List<Activity> get activities => _activities;
+
+  get list => null;
+
+  // Method to fetch activities from the API
+  Future<void> fetchActivities() async {
+    for (int i = 0; i < 5; i++) {
+      final response = await http.get(
+          Uri.parse('https://www.boredapi.com/api/activity/?participants=1'));
+
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.body);
+        _activities.add(Activity.fromJson(jsonData));
+      } else {
+        throw Exception('Failed to load activities');
+      }
+    }
+    notifyListeners(); // Notify listeners (widgets) that data has changed
+  }
+
+  //come to this later
+  void handleDeleteActivity(e) {}
+  //come to this later
+  void handleDeleteAll() {}
+}
+
+//=================================================>>>>
+
+class Page_1 extends StatefulWidget {
   const Page_1({
     super.key,
   });
 
   @override
+  State<Page_1> createState() => _Page_1State();
+}
+
+class _Page_1State extends State<Page_1> {
+  @override
+  void initState() {
+    super.initState();
+    // Fetch activities when the home page is initialized
+    Provider.of<ActivityProvider>(context, listen: false).fetchActivities();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Consumer<ActivityProvider>(
       builder: (context, activityProvider, _) {
-        /*
-                  log(
-                    'isloading: ${activityProvider.isloading}',
-                    name: 'activity',
-                  );
-                  log(
-                    'list: ${activityProvider.list}',
-                    name: 'activity',
-                  );
-                  */
         return Scaffold(
           body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 200,
-                  width: 300,
-                  child: Card(
-                    color: Colors.green,
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Align(
-                          alignment: Alignment.center,
-                          child: activityProvider.isloading
-                              ? const CircularProgressIndicator()
-                              : Text(
-                                  activityProvider.activity,
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .titleLarge
-                                      ?.copyWith(
-                                        color: Colors.white,
-                                      ),
-                                  textAlign: TextAlign.center,
-                                )),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    activityProvider.fetchActivity();
+            child: Consumer<ActivityProvider>(
+              builder: (context, activityProvider, _) {
+                final activities = activityProvider
+                    .activities; // Get the list of activities from the provider
+                return ListView.builder(
+                  itemCount: activities.length,
+                  itemBuilder: (context, index) {
+                    final activity = activities[index];
+                    return ListTile(
+                      title: Text(activities[index].activity),
+                    );
                   },
-                  child: const Text("Next Activity"),
-                ),
-              ],
+                );
+              },
             ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              // Fetch activities when the refresh button is pressed
+              Provider.of<ActivityProvider>(context, listen: false)
+                  .fetchActivities();
+            },
+            child: Icon(Icons.refresh),
           ),
         );
       }, // builder
