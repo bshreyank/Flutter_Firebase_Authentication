970e173c74a8e2efae0297975db60b5dab2e3016
shreyank.bansod <shreyank.bansod@cionlabs.com>
2024-02-16 15:45:56 +0530

    moved the provider funcationality from dashboard to part_1 dart file

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,4 @@
-import 'package:complete/dashboard.dart';
+import 'package:complete/page_1.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'auth_gate.dart';
