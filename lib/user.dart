da753969cf6a316b2e68f2c19732fa36359628d4
shreyank.bansod <shreyank.bansod@cionlabs.com>
2024-02-19 18:15:58 +0530

    freezed-package-example

---
+++ lib/user.dart
@@ -0,0 +1,11 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user.g.dart';
+part 'user.freezed.dart';
+
+@freezed
+class User with _$User {
+  const factory User() = _User;
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+}
---
+++ lib/user.g.dart
@@ -0,0 +1,12 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$UserImpl _$$UserImplFromJson(Map<String, dynamic> json) => _$UserImpl();
+
+Map<String, dynamic> _$$UserImplToJson(_$UserImpl instance) =>
+    <String, dynamic>{};
