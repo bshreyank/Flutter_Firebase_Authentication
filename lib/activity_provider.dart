7c79498ef031571cc33d2aeab1e37ce84a559222
shreyank.bansod <shreyank.bansod@cionlabs.com>
2024-02-20 14:30:10 +0530

    Removed setState Activity and Added Freezed Activity

--- lib/activity_provider.dart
+++ lib/activity_provider.dart
@@ -1,29 +1,14 @@
 import 'dart:convert';
+import 'package:complete/user.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+// import 'package:cloud_firestore/cloud_firestore.dart';
 
-//=================================================>>>>
 //================================================ Provider ====>>>>
-// Represents an activity fetched from the API
-class Activity {
-  final String activity;
-
-  Activity({required this.activity});
-
-  // Factory method to create an Activity object from JSON data
-  factory Activity.fromJson(Map<String, dynamic> json) {
-    return Activity(
-      activity: json['activity'],
-    );
-  }
-}
-
-//===========================================================>>>>
 
 class ActivityProvider extends ChangeNotifier {
   final List<Activity> _activities = [];
   bool _activitiesFetched = false;
-
   List<Activity> get activities => _activities;
 
   //============ Main Bored API fetching start ===>>>>>
@@ -35,7 +20,13 @@ class ActivityProvider extends ChangeNotifier {
 
         if (response.statusCode == 200) {
           final jsonData = json.decode(response.body);
-          _activities.add(Activity.fromJson(jsonData));
+          _activities.add(
+            Activity(
+              title: jsonData['activity'],
+              isLiked: false,
+              isSaved: false,
+            ),
+          );
         } else {
           throw Exception('Failed to load activities');
         }
@@ -45,28 +36,6 @@ class ActivityProvider extends ChangeNotifier {
     }
   }
   //============ Main Bored API fetching end ===>>>>>
-
-  //=========== Adding data to firebase database - start===>>>>
-  final String firebaseUrl =
-      'https://fir-database-8ba40-default-rtdb.firebaseio.com/';
-
-  Future<void> addToFirebase(String key, String value) async {
-    try {
-      await Future.delayed(
-          Duration.zero); // Delay to allow proper widget initialization
-      final response = await http.post(
-        Uri.parse('$firebaseUrl$key.json'),
-        body: jsonEncode(value),
-      );
-
-      if (response.statusCode != 200) {
-        throw Exception('Failed to add data to Firebase: ${response.body}');
-      }
-    } catch (error) {
-      print('Failed to add data to Firebase: $error');
-    }
-  } //Future
-  //=========== Adding data to firebase database - end===>>>>
 
 // =============== Function ========>>>>>
   // Method to fetch activities again
