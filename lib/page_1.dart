14cbcc40706377eecdb65c15b24c6f72e251004c
shreyank.bansod <shreyank.bansod@cionlabs.com>
2024-02-20 18:31:02 +0530

    Implied Saved/NotSaved Fnclty, onPressed also. Connedted to Single Users many activity. Implied StrikeThrough Fnc.

--- lib/page_1.dart
+++ lib/page_1.dart
@@ -1,4 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:complete/page_2.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -18,9 +21,29 @@ class Page_1 extends StatefulWidget {
 //==========================================>>>>
 
 class _Page_1State extends State<Page_1> {
-  //Like and Unlike
-  late int selectedItemIndex;
+  //saved and not saved
+  void toggleSave(String title) {
+    setState(() {
+      final activityProvider =
+          Provider.of<ActivityProvider>(context, listen: false);
+      final index =
+          activityProvider.activities.indexWhere((item) => item.title == title);
 
+      if (index != -1) {
+        bool isSaved = !activityProvider.activities[index].isSaved;
+
+        activityProvider.activities[index] =
+            activityProvider.activities[index].copyWith(isSaved: isSaved);
+
+        FirebaseFirestore.instance
+            .collection(FirebaseAuth.instance.currentUser!.uid)
+            .doc(title)
+            .set(activityProvider.activities[index].toJson());
+      }
+    });
+  }
+
+  //Like and Unlike
   void toggleLike(String title) {
     setState(() {
       final activityProvider =
@@ -33,7 +56,7 @@ class _Page_1State extends State<Page_1> {
 
         // Update Firestore with isLiked status
         FirebaseFirestore.instance
-            .collection('activities')
+            .collection(FirebaseAuth.instance.currentUser!.uid)
             .doc(title)
             .set(activityProvider.activities[index].toJson());
       }
@@ -61,11 +84,22 @@ class _Page_1State extends State<Page_1> {
                 return ListView.builder(
                   itemCount: activities.length,
                   itemBuilder: (context, index) {
+                    //strikethrough functionality
+                    final activity = activities[index];
+                    final bool isLikedAndSaved =
+                        activity.isLiked && activity.isSaved;
                     return Row(
                       children: [
                         Expanded(
                           child: ListTile(
-                            title: Text(activities[index].title),
+                            title: Text(
+                              activities[index].title,
+                              style: TextStyle(
+                                decoration: isLikedAndSaved
+                                    ? TextDecoration.lineThrough
+                                    : TextDecoration.none,
+                              ),
+                            ),
                           ),
                         ),
                         IconButton(
@@ -97,12 +131,26 @@ class _Page_1State extends State<Page_1> {
                         IconButton(
                           onPressed: () {
                             // Pass the selected activity to Page_2
-                            activityProvider
-                                .addsetSelectedActivity(activities[index]);
+                            //activityProvider.addsetSelectedActivity(activities[index]);
                             // Navigate to Page_2
-                            Navigator.pushNamed(context, '/page2');
+                            //Navigator.pushNamed(context, '/page2');
+                            //===============>>>>
+                            String title = activities[index].title;
+                            toggleSave(title);
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                    activityProvider.activities[index].isSaved
+                                        ? 'Activity saved!'
+                                        : 'Activity removed from saved!'),
+                              ),
+                            );
                           },
-                          icon: Icon(Icons.add),
+                          icon: Icon(
+                            activities[index].isSaved ? Icons.check : Icons.add,
+                            color:
+                                activities[index].isSaved ? Colors.green : null,
+                          ),
                         ),
                       ],
                     );
