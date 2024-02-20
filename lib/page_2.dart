9bc26f723170a77b5f045536cbd971014f8c5f3c
shreyank.bansod <shreyank.bansod@cionlabs.com>
2024-02-20 18:34:50 +0530

    Removed all the setState fnclty. Added Database fetching Display Data. Implied Delete single and All Data.

--- lib/page_2.dart
+++ lib/page_2.dart
@@ -1,44 +1,88 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'activity_provider.dart';
 
 class Page_2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer<ActivityProvider>(
-      builder: (context, activityProvider, _) {
-        final selectedActivities = activityProvider.selectedActivities;
-
-        return Scaffold(
-          appBar: AppBar(
-            title: Text('Selected Activities'),
-          ),
-          body: ListView.builder(
-            itemCount: selectedActivities.length,
+    return Scaffold(
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection(FirebaseAuth.instance.currentUser!.uid)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return LinearProgressIndicator();
+          }
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          }
+          final List<DocumentSnapshot> documents = snapshot.data!.docs;
+          return ListView.builder(
+            itemCount: documents.length,
             itemBuilder: (context, index) {
-              final activity = selectedActivities[index];
+              final data = documents[index].data() as Map<String, dynamic>;
+              final title =
+                  data['title'] ?? 'fff'; // Handle potential null values
+
               return ListTile(
-                title: Text(activity.title),
-                trailing: IconButton(
-                  icon: Icon(Icons.delete),
-                  onPressed: () {
-                    // Delete the activity
-                    activityProvider.deleteActivity(activity);
-                  },
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      title,
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () {
+                        // Delete the activity
+                        FirebaseFirestore.instance
+                            .collection(FirebaseAuth.instance.currentUser!.uid)
+                            .doc(documents[index].id)
+                            .delete()
+                            .then(
+                          (_) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text('Activity Deleted'),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    ),
+                  ],
                 ),
               );
             },
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              // Delete all activities
-              activityProvider.deleteAllActivities();
-            },
-            child: Icon(Icons.delete_forever),
-            tooltip: 'Delete All',
-          ),
-        );
-      },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //Delete All Data
+          FirebaseFirestore.instance
+              .collection(FirebaseAuth.instance.currentUser!.uid)
+              .get()
+              .then((snapshot) {
+            for (DocumentSnapshot ds in snapshot.docs) {
+              ds.reference.delete();
+            }
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('All activities deleted'),
+              ),
+            );
+          }).catchError((error) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('Failed to delete activities: $error'),
+              ),
+            );
+          });
+        },
+        child: Icon(Icons.delete_forever),
+      ),
     );
   }
 }
